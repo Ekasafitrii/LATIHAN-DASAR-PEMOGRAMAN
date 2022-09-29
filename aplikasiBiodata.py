ee782cfe03ed90d8cb083fcae4d63e0cf0267a0e
Ekasafitrii <114630015+Ekasafitrii@users.noreply.github.com>
2022-09-29 11:10:39 +0700

    Add files via upload

---
+++ aplikasiBiodata.py
@@ -0,0 +1,15 @@
+#aplikasi biodata creat by eka safitri 
+
+#memasukan nama 
+print ("yuk lengkapi data diri anda..")
+
+#input Data Diri
+nama = input("nama         : ")
+kelas = input("kelas       : ")
+prodi = input("program studi : ")
+
+print("Apakah data di bawah ini sudah benar ? ")
+
+#print / Output Data Diri
+print("Nama anda adalah: ", nama)
+print("kelas anda adalah: ", kelas)
